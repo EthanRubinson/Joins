4cc0125c576080e44f72ec9e72598b888dc307df
Ethan Rubinson <ethan@tebrotech.com>
2014-12-05 16:00:00 -0500

    Block and Index Nested Loops Done

--- Joins/src/BlockNestedLoops.cpp
+++ Joins/src/BlockNestedLoops.cpp
@@ -19,6 +19,103 @@
 //---------------------------------------------------------------
 Status BlockNestedLoops::Execute(JoinSpec& left, JoinSpec& right, JoinSpec& out) {
 	JoinMethod::Execute(left, right, out);
-	//Your code here. 
-	return FAIL;
+	
+	//	Create the temporary heapfile
+	Status st;
+	HeapFile *tmpHeap = new HeapFile(NULL, st);
+	if (st != OK) {
+		std::cerr << "Failed to create output heapfile." << std::endl;
+		return FAIL;
+	}
+
+	//	Open scan on left relation
+	Status leftStatus;
+	Scan *leftScan = left.file->OpenScan(leftStatus);
+	if (leftStatus != OK) {
+		std::cerr << "Failed to open scan on left relation." << std::endl;
+		return FAIL;
+	}
+
+	//	Open scan on right relation
+	Status rightStatus;
+	Scan *rightScan = right.file->OpenScan(rightStatus);
+	if (rightStatus != OK) {
+		std::cerr << "Failed to open scan on right relation." << std::endl;
+		return FAIL;
+	}
+
+	/*  Simulate a "block" with an array for storing records.
+	 *  The amount of memory required is the size of a record
+	 *  times the amount of records that can fit in a block
+	 *
+	 *  Since we can assume the left relation has <= the number
+	 *  of values than the right, we want to use this block to
+	 *  store the records of the left relation
+	 */
+	char *block = new char[left.recLen * blockSize];
+	int recordsInBlock = 0;
+	
+	RecordID firstRightScanRid = rightScan->currRid;
+	leftStatus = DONE;
+	char *leftRec  = new char[left.recLen];
+	while (true) {
+		//  For each block b in R, read block b into an array
+		while (recordsInBlock < blockSize) {
+			RecordID leftRid;
+			leftStatus = leftScan->GetNext(leftRid, leftRec, left.recLen);
+			if (leftStatus == OK) {
+				memcpy(block + (left.recLen * recordsInBlock), leftRec, left.recLen);
+				recordsInBlock++;
+			}
+			else if (leftStatus == DONE) break;
+			else return FAIL;
+		}
+
+		//  For each tuple s in S
+		RecordID rightRid;
+		char *rightRec = new char[right.recLen];
+		while (true) {
+			rightStatus = rightScan->GetNext(rightRid, rightRec, right.recLen);
+			if (rightStatus == DONE) break;
+			if (rightStatus != OK) return FAIL;
+
+			int *rightJoinValPtr = (int*)(rightRec + right.offset);
+			//  For each tuple r in b
+			for (int index = 0; index < recordsInBlock; index++) {
+				char *leftRec = (block + (left.recLen * index));
+				int *leftJoinValPtr = (int*)(leftRec + left.offset);
+				
+				//  Match r with s
+				if (*leftJoinValPtr == *rightJoinValPtr) {
+					//  If Match then Insert (r,s)
+					char *joinedRec = new char[out.recLen];
+					MakeNewRecord(joinedRec, leftRec, rightRec, left, right);
+					RecordID insertedRid;
+					Status tmpStatus = tmpHeap->InsertRecord(joinedRec, out.recLen, insertedRid);
+					
+					if (tmpStatus != OK) {
+						std::cerr << "Failed to insert tuple into output heapfile." << std::endl;
+						return FAIL;
+					}
+					
+					delete [] joinedRec;
+				}
+			}
+		}
+		delete [] rightRec;
+
+		if (leftStatus == DONE) break;
+
+		rightScan->MoveTo(firstRightScanRid);
+		recordsInBlock = 0;
+	}
+
+	out.file = tmpHeap;
+
+	delete [] leftRec;
+	delete [] block;
+	delete leftScan;
+	delete rightScan;
+
+	return OK;
 }
--- Joins/src/IndexNestedLoops.cpp
+++ Joins/src/IndexNestedLoops.cpp
@@ -24,5 +24,108 @@
 //---------------------------------------------------------------
 Status IndexNestedLoops::Execute(JoinSpec& left, JoinSpec& right, JoinSpec& out) {
 	JoinMethod::Execute(left, right, out);
-	return FAIL;
+
+	//	Create the temporary heapfile
+	Status st;
+	HeapFile *tmpHeap = new HeapFile(NULL, st);
+	if (st != OK) {
+		std::cerr << "Failed to create output heapfile." << std::endl;
+		return FAIL;
+	}
+
+	//	Open scan on left relation
+	Status leftStatus;
+	Scan *leftScan = left.file->OpenScan(leftStatus);
+	if (leftStatus != OK) {
+		std::cerr << "Failed to open scan on left relation." << std::endl;
+		return FAIL;
+	}
+
+	//	Open scan on right relation
+	Status rightStatus;
+	Scan *rightScan = right.file->OpenScan(rightStatus);
+	if (rightStatus != OK) {
+		std::cerr << "Failed to open scan on right relation." << std::endl;
+		return FAIL;
+	}
+
+	//  Create the unclustered B+ tree index
+	Status treeStatus;
+	BTreeFile *tree = new BTreeFile(treeStatus, "BPlusTreeIndex");
+	if (treeStatus != OK) {
+		std::cerr << "Failed to create B+ tree index." << std::endl;
+		return FAIL;
+	}
+
+	//  Populate the tree with the records from the right relation
+	char *rightKey = new char[9];
+	char *rightRec  = new char[right.recLen];
+	while (true){
+		RecordID rightRid;
+		rightStatus = rightScan->GetNext(rightRid, rightRec, right.recLen);
+		if (rightStatus == DONE) break;
+		if (rightStatus != OK) return FAIL;
+
+		//	The join attribute on right relation
+		int *rightJoinValPtr = (int*)(rightRec + right.offset);
+
+		toString(*rightJoinValPtr, rightKey);
+
+		if (tree->Insert(rightKey, rightRid) != OK) return FAIL;
+	}
+
+	//  Search the B+ tree index for the keys in the left relation
+	char *leftKey = new char[9];
+	char *leftRec = new char[left.recLen];
+	while (true){
+		RecordID leftRid;
+		leftStatus = leftScan->GetNext(leftRid, leftRec, left.recLen);
+		if (leftStatus == DONE) break;
+		if (leftStatus != OK) return FAIL;
+
+		//	The join attribute on left relation
+		int *leftJoinValPtr = (int*)(leftRec + left.offset);
+
+		toString(*leftJoinValPtr, leftKey);
+
+		//  Scan the B+ tree index for matches on the leftKey
+		BTreeFileScan *btScan = tree->OpenScan(leftKey, leftKey);
+		while (true){
+			char *tmpKey = NULL;
+			RecordID rid;
+			Status btreeStatus = btScan->GetNext(rid, tmpKey);
+			if (btreeStatus == DONE) break;
+			if (btreeStatus != OK) return FAIL;
+
+			right.file->GetRecord(rid, rightRec, right.recLen);
+
+			char *joinedRec = new char[out.recLen];
+			MakeNewRecord(joinedRec, leftRec, rightRec, left, right);
+			RecordID insertedRid;
+			Status tmpStatus = tmpHeap->InsertRecord(joinedRec, out.recLen, insertedRid);
+
+			if (tmpStatus != OK) {
+				std::cerr << "Failed to insert tuple into output heapfile." << std::endl;
+				return FAIL;
+			}
+					
+			delete [] joinedRec;
+		}
+	}
+
+	out.file = tmpHeap;
+	
+	delete leftScan;
+	delete rightScan;
+
+	delete [] leftRec;
+	delete leftKey;
+
+	delete [] rightRec;
+	delete rightKey;
+
+	tree->DestroyFile();
+	delete tree;
+
+	return OK;
 }
